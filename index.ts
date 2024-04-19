a7090b028c628a78aacd7bf1fbde1c19a610536e
nidatariq128 <167254790+nidatariq128@users.noreply.github.com>
2024-04-19 16:10:22 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+//import the "inquirer" module ,which is a comand line interface for node.js:
+import inquirer  from "inquirer"
+
+// declear a constant `answer`and assign it to the result of inquirer.prompt function:
+
+
+
+const answer:{
+  sentance: string
+} = await inquirer.prompt([
+{
+    name:"sentance",
+    type:"input",
+    message:"enter your scentance to count the word:",
+}
+])
+// trim space ko  count krta h/split latter or space dono ko count krta h:
+const words = answer.sentance.trim().split(" ")
+
+//print the array of words to the console:
+console.log(words)
+
+//print the word count of the sentance to the console:
+console.log(`your sentance word count is ${words.length}`)
